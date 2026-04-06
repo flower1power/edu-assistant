c2c2d04d8524732211375787eaf8c824973db0f2
Viktor.Lobyntsev <sibfight@mail.ru>
2026-04-06 23:18:13 +0300

    add demo.html and api endpoint /demo

--- src/edu_assistant/api.py
+++ src/edu_assistant/api.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI, Form, HTTPException
+from fastapi.responses import FileResponse
 from openai import OpenAIError
 
 from edu_assistant.assistant import create_response
 from edu_assistant.config import RoleType, TemplateType
 
 app = FastAPI()
+
+
+@app.get('/demo')
+def demo():
+    return FileResponse("templates/demo.html")
 
 
 @app.post('/ask')
